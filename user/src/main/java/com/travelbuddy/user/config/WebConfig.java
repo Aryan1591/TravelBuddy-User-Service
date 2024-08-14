59062f7798cf7f4b83e067ca04b80de270552e11
Aryan1591 <54468277+Aryan1591@users.noreply.github.com>
2024-08-15 02:07:20 +0530

    Update WebConfig.java

--- user/src/main/java/com/travelbuddy/user/config/WebConfig.java
+++ user/src/main/java/com/travelbuddy/user/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Replace with your frontend URL
+                        .allowedOrigins("http://localhost:5173","https://66bd09f19028ae026b5372d9--courageous-bublanina-dd5958.netlify.app","https://travelbuddy-posts-service-production.up.railway.app") // Replace with your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
