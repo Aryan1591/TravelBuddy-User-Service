8b2e46f3272f7f5698df2020956227a9e2953ad1
ABHI-742 <abhisreekrishna742@gmail.com>
2024-09-15 17:59:21 -0400

    added RSA JWT

--- user/src/main/java/com/travelbuddy/user/utils/JwtUtils.java
+++ user/src/main/java/com/travelbuddy/user/utils/JwtUtils.java
@@ -4,13 +4,13 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,25 +18,38 @@ import java.util.Map;
 @Component
 @Getter
 public class JwtUtils {
-     @Value("${security.secretToken}")
-     private String SECRET_TOKEN;
 
-     private String createJWTToken(Map<String, Object> claims, String userName) {
-         return Jwts.builder()
-                 .setClaims(claims).setSubject(userName).setIssuedAt(new Date(System.currentTimeMillis()))
-                 .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60))
-                 .signWith(getSignature(), SignatureAlgorithm.HS256).compact();
-     }
+    private final JwtEncoder jwtEncoder;
+    private final JwtDecoder jwtDecoder;
+    private final RSAPublicKey publicKey;
+    private final RSAPrivateKey privateKey;
 
-     public String generateJWTToken(String userName) {
-         return createJWTToken(new HashMap<String,Object>(), userName);
-     }
-     private Key getSignature() {
-         byte[] keyBytes = Decoders.BASE64.decode(SECRET_TOKEN);
-         return Keys.hmacShaKeyFor(keyBytes);
-     }
+    // Constructor Injection to avoid circular dependencies
+    public JwtUtils(JwtEncoder jwtEncoder, JwtDecoder jwtDecoder, RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+        this.jwtEncoder = jwtEncoder;
+        this.jwtDecoder = jwtDecoder;
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
 
-     public Jws<Claims> validateJWTToken(String token) {
-         return Jwts.parserBuilder().setSigningKey(getSignature()).build().parseClaimsJws(token);
-     }
+    public String createJWTToken(Map<String, Object> claims, String userName) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userName)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(privateKey, SignatureAlgorithm.RS256)
+                .compact();
+    }
+
+    public String generateJWTToken(String userName) {
+        return createJWTToken(new HashMap<>(), userName);
+    }
+
+    public Jws<Claims> validateJWTToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(publicKey)
+                .build()
+                .parseClaimsJws(token);
+    }
 }
